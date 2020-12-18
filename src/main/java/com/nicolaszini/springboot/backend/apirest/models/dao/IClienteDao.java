824a4d85cde080ade19cc91b9538728e15c7288a
unknown <nicolaszini8@gmail.com>
2020-12-17 21:33:06 -0300

    first commit

---
+++ src/main/java/com/nicolaszini/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,13 @@
+package com.nicolaszini.springboot.backend.apirest.models.dao;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.nicolaszini.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
+
+	
+}
